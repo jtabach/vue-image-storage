266fb0d53e460267d0af43c82e49afa40d887153
jtabach <jeff.n.tabachnick@gmail.com>
2019-03-15 10:19:56 -0500

    upload mulitple images

--- src/components/UploadForm.vue
+++ src/components/UploadForm.vue
@@ -1,9 +1,38 @@
 <template>
-  <div>Upload Form</div>
+  <div class="dropper">
+    <input type="file" multiple @change="uploadImages($event.target.files)" accept="image/*">
+    <span>Drag files here!</span>
+  </div>
 </template>
 
 <script>
+import { mapActions } from "vuex";
+
 export default {
-  name: "UploadForm"
+  name: "UploadForm",
+  methods: {
+    ...mapActions(["uploadImages"])
+  }
 };
 </script>
+
+<style scoped>
+.dropper {
+  position: relative;
+  height: 30vh;
+  border: 2px dashed black;
+  border-radius: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.dropper:hover {
+  background-color: #eee;
+}
+input {
+  width: 100%;
+  height: 30vh;
+  position: absolute;
+  opacity: 0;
+}
+</style>
