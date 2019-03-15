9786c067b8e7beaa667cf67eabb4b9b10172cdf9
jtabach <jeff.n.tabachnick@gmail.com>
2019-03-14 22:34:50 -0500

    add vue-router and authentication

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,35 @@
 <template>
   <div class="ui secondary pointing menu">
     <a href="/" class="active item">Image Storage</a>
+    <div class="right menu">
+      <div v-if="isLoggedIn" class="horizontal">
+        <a class="item">Gallery</a>
+        <a class="item">Upload</a>
+        <a class="item" @click="logout">Logout</a>
+      </div>
+
+      <a v-else href="#" class="ui item" @click="login">Login</a>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapActions, mapGetters } from "vuex";
+
 export default {
-  name: "AppHeader"
+  name: "AppHeader",
+  computed: {
+    ...mapGetters(["isLoggedIn"])
+  },
+  methods: {
+    ...mapActions(["login", "logout"])
+  }
 };
 </script>
+
+<style scoped>
+.horizontal {
+  display: flex;
+  flex-direction: row;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
 import Vue from 'vue';
-import App from './App';
+import VueRouter from 'vue-router';
 
+import App from './App';
+import AuthHandler from './components/AuthHandler';
 import store from './store';
 
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+  mode: 'history',
+  routes: [{ path: '/oauth2/callback', component: AuthHandler }]
+});
+
 new Vue({
+  router,
   store,
   render: (h) => h(App)
 }).$mount('#app');
